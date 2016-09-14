064ccbd481fbde5cf2aae298d1198854cbc91d83
RobertoSevero <roberto.severo@outlook.com>
2016-09-13 22:01:47 -0300

    Teste Roberto 3

--- app/src/main/java/br/com/projeto/projeto/Teste.java
+++ app/src/main/java/br/com/projeto/projeto/Teste.java
@@ -4,4 +4,8 @@ package br.com.projeto.projeto;
  * Created by SirKarsh on 13/09/2016.
  */
 public class Teste {
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }

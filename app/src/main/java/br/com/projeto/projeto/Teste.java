eb69280c0be41e24a6ed84f482f645a2d64c9f16
RobertoSevero <roberto.severo@outlook.com>
2016-09-15 20:12:46 -0300

    Teste Roberto 5 Agora vai

--- app/src/main/java/br/com/projeto/projeto/Teste.java
+++ app/src/main/java/br/com/projeto/projeto/Teste.java
@@ -1,5 +1,8 @@
 package br.com.projeto.projeto;
 
+import android.content.Intent;
+import android.view.View;
+
 /**
  * Created by SirKarsh on 13/09/2016.
  */
@@ -7,5 +10,13 @@ public class Teste {
     @Override
     protected Object clone() throws CloneNotSupportedException {
         return super.clone();
-    }
+
+        btn_relatorio.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, RelatorioActivity.class);
+                startActivity(intent);
+
+            }
 }
